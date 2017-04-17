fa67f3843d075c3881a511f075b9989cc49c6166
Erin Bergman <ebergman@pillartechnology.com>
2017-04-17 09:42:01 -0400

    Change protracter.conf.js for mocha & chai

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,7 +1,7 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-const { SpecReporter } = require('jasmine-spec-reporter');
+// const { SpecReporter } = require('jasmine-spec-reporter');
 
 exports.config = {
   allScriptsTimeout: 11000,
@@ -13,18 +13,31 @@ exports.config = {
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
-  framework: 'jasmine',
-  jasmineNodeOpts: {
-    showColors: true,
-    defaultTimeoutInterval: 30000,
-    print: function() {}
+  // framework: 'jasmine',
+  framework: 'mocha',
+  // jasmineNodeOpts: {
+  //   showColors: true,
+  //   defaultTimeoutInterval: 30000,
+  //   print: function() {}
+  // },
+  mochaOpts: {
+    reporter: "spec",
+    slow: 3000,
+    ui: 'bdd',
+    timeout: 30000
   },
-  beforeLaunch: function() {
+  beforeLaunch: function () {
     require('ts-node').register({
       project: 'e2e/tsconfig.e2e.json'
     });
   },
-  onPrepare() {
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+  // onPrepare() {
+  //   jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+  // }
+  onPrepare: function() {
+    var chai = require('chai');
+    var chaiAsPromised = require("chai-as-promised");
+    chai.use(chaiAsPromised);
+    global.chai = chai;
   }
 };
