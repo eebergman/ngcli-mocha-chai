94a8a193c6b0ab9fbd965e69c83d16f0bc7f9b30
Erin Bergman <ebergman@pillartechnology.com>
2017-04-17 10:30:43 -0400

    Change Jasmine syntax to mocha/chai in arc/app/app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed, async } from '@angular/core/testing';
+import { expect } from 'chai';
 
 import { AppComponent } from './app.component';
 
@@ -14,19 +15,20 @@ describe('AppComponent', () => {
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    expect(app).to.be.true;
   }));
 
   it(`should have as title 'app works!'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
+    expect(app.title).to.equal('app works!');
   }));
 
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    const content = compiled.querySelector('h1').textContent;
+    expect(content).to.include('app works!');
   }));
 });
